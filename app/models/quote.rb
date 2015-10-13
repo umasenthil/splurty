040a0a404ad871cf1e429f26ad3e88ca39295a95
uma senthil <uma_senthil@hotmail.com>
2015-10-13 02:21:27 +0000

    Add modal for contribute popup

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,3 @@
 class Quote < ActiveRecord::Base
-	validates :saying, :presence => true, :length => { :maximum => 140, :minimum => 3 }
+	validates :saying, :presence => true, :length => { :maximum => 500, :minimum => 3 }
 end
